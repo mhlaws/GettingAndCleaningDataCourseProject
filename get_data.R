152a3714610e72c006ca98d5ca3dac83d7ced7aa
Michael Laws <mhlaws282@gmail.com>
2015-07-26 11:22:18 -0400

    getting and cleaning data - course project - init

---
+++ get_data.R
@@ -0,0 +1,6 @@
+# description of the data: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+if(!file.exists("./data")) dir.create("./data")
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileURL, destfile = "./data/data.zip", method = "curl")
+unzip("./data/data.zip", exdir = "./data")
